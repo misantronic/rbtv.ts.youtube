6701a7d8ceceab1c5a1474fc3ab80c577207fe94
David Schkalee <david.schkalee@sysvision.de>
2017-07-31 09:27:37 +0200

    feat: Add shows-util

---
+++ src/utils/shows.ts
@@ -0,0 +1,132 @@
+import {channel} from './channels'
+
+export interface Show {
+    title: string;
+    channel: channel,
+    playlistId?: string
+};
+
+export const shows: Show[] = [
+    {
+        title: 'MoinMoin',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3KZe3wbmAP2Tgn6rfhbDlBH'
+    },
+    {
+        title: '1on1',
+        channel: channel.RBTV
+    },
+    {
+        title: 'Kino+',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3KtrWWeupRy5h0Di7N_MufB'
+    },
+    {
+        title: 'Bohn Jour',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Kxtqnu9Ht8b2XB_HGUYVJ1'
+    },
+    {
+        title: 'Almost Daily',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3I6-7OYZ0GigNnc7KI5S0OK'
+    },
+    {
+        title: 'Almost Plaily',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Ly_8f3RTw6MU_2Eg-2xMNA'
+    },
+    {
+        title: 'Game+',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3K4w-99v-cmC5NSqPbAGjTz'
+    },
+    {
+        title: 'Game+ Daily',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Lrl-mQWnuIiffBAg1fCxwz'
+    },
+    {
+        title: 'Pen & Paper: T.E.A.R.S.',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3I3cY9vXrygdSqavv_U_0oZ'
+    },
+    {
+        title: 'Pen & Paper: B.E.A.R.D.S.',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Kk6Uz2NCG4QAjhKy4y8T5'
+    },
+    {
+        title: 'Panelz',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3I8ImSEa98WCr73iu3vjpn4'
+    },
+    {
+        title: 'Chat Duell',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3JFM4GKuRmWpIgxl5QcMwO0'
+    },
+    {
+        title: 'TheraThiel',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3J1z9gC0kp9l7TfwSCSjCfe'
+    },
+    {
+        title: 'Bohndesliga',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3IDcHQVTbGpfEdNexUFy_pb'
+    },
+    {
+        title: 'Spiele mit Bart',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Ilo0vc3S5owjkmUV6XtH6T'
+    },
+    {
+        title: 'RBTV News',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3JVqDJW3C92GaX_mEwiAAKK'
+    },
+    {
+        title: 'Nerd Quiz S01',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3J9TYKS2-UrSvwrU1jUfU2y'
+    },
+    {
+        title: 'Nerd Quiz S02',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3LQKx_i3iTwmmfCP8z9uIk5'
+    },
+    {
+        title: 'Press Select',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3LZ4CPFFfWqZp2WMDAmd7Om'
+    },
+    {
+        title: 'Let\'s Play',
+        channel: channel.LP
+    },
+    {
+        title: 'BEARDS',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3Kk6Uz2NCG4QAjhKy4y8T50'
+    },
+    {
+        title: 'Ranked',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3JlEwshL7tSgdEGuHBFEgUy'
+    },
+    {
+        title: 'Team Limited Counter-Strike:Global Offensive',
+        channel: channel.LP,
+        playlistId: 'PLfU2RMxoOiSCT4dEz-KxCi7kqiN6YQnCS'
+    },
+    {
+        title: 'Wir müssen reden',
+        channel: channel.RBTV,
+        playlistId: 'PLsksxTH4pR3K77xBcusJB9hmjUyodWdUo'
+    },
+    {
+        title: 'Game Two',
+        channel: channel.G2
+    }
+];
