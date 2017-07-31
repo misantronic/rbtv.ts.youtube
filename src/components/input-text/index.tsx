bbe1f20df87cc4cdaa93651ee8aab77af370780d
David Schkalee <david.schkalee@sysvision.de>
2017-07-31 09:28:30 +0200

    refactor: Rewrite InputText-component

--- src/components/input-text/index.tsx
+++ src/components/input-text/index.tsx
@@ -1,27 +1,62 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-interface InputTextProps {
+type Border = 'default' | 'none' | 'error';
+
+export interface InputTextProps {
     value: string;
     className?: string;
     placeholder?: string;
+    border?: Border;
+    padding?: string;
+    autofocus?: boolean;
     onChange(val: string): void;
+    onKeyDown?(e: any): void;
 }
 
+const styles = {
+    border: (props: InputTextProps): string => {
+        switch (props.border) {
+            case 'error':
+                return '1px solid red';
+            case 'none':
+                return 'none';
+            default:
+                return '1px solid #ccc';
+        }
+    }
+};
+
 const Input = styled.input`
-    padding: 8px 12px;
     width: 100%;
-    font-size: 14px;
+    height: 36px;
+    padding: 8px 12px;
+    border: ${styles.border};
 
     &:focus {
         outline: none;
     }
-`
+`;
 
-export class InputText extends React.Component<InputTextProps> {
+export class InputText extends React.PureComponent<InputTextProps> {
+    static defaultProps = {
+        border: 'default'
+    };
+
     render(): JSX.Element {
-        const { value, onChange, className, placeholder } = this.props;
-
-        return <Input type="text" className={className} placeholder={placeholder} value={value} onChange={(e: any) => onChange(e.target.value)} />;
+        const { value, onChange, onKeyDown, className, placeholder, padding, autofocus } = this.props;
+        
+        return (
+            <Input
+                type="text"
+                className={className}
+                placeholder={placeholder}
+                value={value}
+                padding={padding}
+                autofocus={autofocus}
+                onChange={(e: any) => onChange(e.target.value)}
+                onKeyDown={onKeyDown}
+            />
+        );
     }
 }
