bb8f8d0dd36c59124aeb7af7e85abe56dfd4ee00
David Schkalee <david.schkalee@sysvision.de>
2017-08-14 22:48:13 +0200

    fix: remove import

--- src/components/responsive-column/index.tsx
+++ src/components/responsive-column/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled, { css, ThemedCssFunction } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 type sizes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
