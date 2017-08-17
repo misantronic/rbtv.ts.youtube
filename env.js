4f4e5e9841726bb3e1c2ea282ea3b49a6c09174f
David Schkalee <david.schkalee@sysvision.de>
2017-08-17 03:08:15 +0200

    fix: remove env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-const REDIS_URL = 'redis://h:p5vr73nd184be5d4qnf3fvj7les@ec2-46-137-183-71.eu-west-1.compute.amazonaws.com:7899';
-const MONGOLAB_URI = 'mongodb://heroku_7j52j90v:4751ln3jfquffg3lg2i3eopcgq@ds013280.mlab.com:13280/heroku_7j52j90v';
-const YT_KEY = 'AIzaSyD0WjzJ5761EemQ-lVor5er2JLR3PJGsGk';
-
-module.exports = {
-    REDIS_URL,
-    MONGOLAB_URI,
-    YT_KEY
-};
